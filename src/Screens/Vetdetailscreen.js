757223fffc0f606fa7c3f47e149eaf29a8abd97a
snapper <zihanjashim2121@gmail.comm>
2022-07-28 14:08:11 +0600

    vet details updated

--- src/Screens/Vetdetailscreen.js
+++ src/Screens/Vetdetailscreen.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from '../Components/Header'
+import {
+  BsFacebook,
+  BsWhatsapp,
+  BsTelegram,
+  BsTelephoneFill,
+} from 'react-icons/bs'
+import { HiMailOpen } from 'react-icons/hi'
 
 const Vetdetailscreen = () => {
   const { vetId } = useParams()
@@ -14,47 +21,93 @@ const Vetdetailscreen = () => {
       .then((data) => setVet(data))
   }, [vetId])
 
+  const address = vet.address
+  const social = vet.social
+
   return (
     <>
       <Header />
-      <div className='container p-10 m-10'>
-        <div className='grid grid-cols-3 gap-4 p-2'>
-          <div className='...'>
-            <img src={vet.img} alt='' />
-          </div>
-          <div className='col-span-2'>
-            <div className='grid grid-cols-2 gap-4 p-2'>
-              <div className=''>
-                <label className='font-semibold'>Name</label>
-              </div>
-              <div className=''>
-                <p>{vet.name}</p>
-              </div>
-            </div>
-            <div className='grid grid-cols-2 gap-4 p-2'>
-              <div className=''>
-                <label className='font-semibold'>Email</label>
-              </div>
-              <div className=''>
-                <p>{vet.email}</p>
-              </div>
-            </div>
-            <div className='grid grid-cols-2 gap-4 p-2'>
-              <div className=''>
-                <label className='font-semibold'>Phone</label>
-              </div>
-              <div className=''>
-                <p>{vet.contact}</p>
-              </div>
-            </div>
+      <h1 className='font-black text-transparent text-4xl bg-clip-text bg-gradient-to-r from-green-400 to-blue-600 text-center bangla-font p-2 m-2'>
+        Details About : <span className='text-gray-800'>{vet.name} </span>
+      </h1>
 
-            <div className='grid grid-cols-2 gap-4 p-2'>
-              <div className=''>
-                <label className='font-semibold'>Facebook</label>
-              </div>
-              <div className=''>
-                <a href='www://facebook.com'>{vet.name}</a>
-              </div>
+      <div class='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 p-5'>
+        <div class='grid grid-rows-2 grid-flow-col'>
+          <div className='flex items-center justify-center '>
+            <img className='w-3/6' src={vet.img} alt='' />
+          </div>
+          <div className='flex justify-center mt-5 space-x-4'>
+            <a
+              href={social?.whatsapp}
+              className='cursor-pointer text-blue-900'
+            >
+              <BsWhatsapp size={25} />
+            </a>
+            <a
+              href={social?.telegram}
+              className='cursor-pointer text-blue-900'
+            >
+              <BsTelegram size={25} />
+            </a>
+            <a
+              href={vet.contact}
+              className='cursor-pointer text-blue-900'
+            >
+              <BsTelephoneFill size={25} />
+            </a>
+            <a
+              href={social?.facebook}
+              className=' cursor-pointer text-blue-900'
+            >
+              <BsFacebook size={25} />
+            </a>
+            <a
+              href={vet.email}
+              className='cursor-pointer text-blue-900'
+            >
+              <HiMailOpen size={25} />
+            </a>
+          </div>
+          
+        </div>
+
+        <div className='header-font'>
+          <div className='grid grid-cols-2 gap-4 p-2'>
+            <div>
+              <p className='font-semibold text-xl'>Name</p>
+            </div>
+            <div>
+              <p className='text-xl'>{vet.name}</p>
+            </div>
+            <div>
+              <p className='font-semibold text-xl'>Email</p>
+            </div>
+            <div>
+              <p className='text-xl'>{vet.email}</p>
+            </div>
+            <div>
+              <p className='font-semibold text-xl'>Contact</p>
+            </div>
+            <div>
+              <p className='text-xl'>{vet.contact}</p>
+            </div>
+            <div>
+              <p className='font-semibold text-xl'>Division</p>
+            </div>
+            <div>
+              <p className=' text-xl'>{address?.division}</p>
+            </div>
+            <div>
+              <p className='font-semibold text-xl'>District</p>
+            </div>
+            <div>
+              <p className=' text-xl'>{address?.district}</p>
+            </div>
+            <div>
+              <p className='font-semibold text-xl'>upzila</p>
+            </div>
+            <div>
+              <p className='text-xl'>{address?.upazila}</p>
             </div>
           </div>
         </div>
